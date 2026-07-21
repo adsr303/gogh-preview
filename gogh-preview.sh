3465986e7d0c06b33d32fdad99b94e2d2f9e4cf0
Artur Rocha <adsr@poczta.onet.pl>
2026-07-21 23:15:46 +0200

    Basic matrix demo

--- gogh-preview.sh
+++ gogh-preview.sh
@@ -4,13 +4,14 @@
 
 # Working with stdin for now
 
+declare -A THEME_COLORS
+
 parse-line() {
-    local LINE="$1"
-    case "$LINE" in
-        color_[01][0-9]:* | background:* | foreground:* | cursor:* )
-            local PARTS
-            read -r -a PARTS <<< "$LINE"
-            declare -gu "THEME_${LINE%%:*}"="${PARTS[1]:3:6}"
+    local PROPERTY="${1%:*}"
+    local VALUE="$2"
+    case "$PROPERTY" in
+        color_[01][0-9] | background | foreground | cursor)
+            THEME_COLORS+=(["$PROPERTY"]="${VALUE:2:6}")
             ;;
     esac
 }
@@ -29,4 +30,25 @@ color-print() {
 }
 
 show() {
+    # TODO:
+    # 1. Switch to alternate screen \033[?1049h
+    # 2. Display theme simulation in the center of the screen
+    # 3. Wait for any key press
+    IFS='' read -r -n 1 -s
+    # 4. Restore normal screen \033[?1049l
+    # See https://gist.github.com/ConnerWill/d4b6c776b509add763e17f9f113fd25b
 }
+
+while read -r -a PARTS; do
+    parse-line "${PARTS[@]}"
+done
+
+echo "${!THEME_COLORS[@]}"
+for i in {01..16}; do
+    FG="${THEME_COLORS["color_${i}"]}"
+    for j in {01..16}; do
+        BG="${THEME_COLORS["color_${j}"]}"
+        color-print "$FG" "$BG" " $i $j "
+    done
+    echo
+done
