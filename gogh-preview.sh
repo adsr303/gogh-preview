585d4b46a41289cedb1c7171f513f9ec6428dabd
Artur Rocha <adsr@poczta.onet.pl>
2026-07-22 19:48:36 +0200

    Show theme name

--- gogh-preview.sh
+++ gogh-preview.sh
@@ -6,11 +6,16 @@
 
 declare -A THEME_COLORS
 
-while read -r PROPERTY_C VALUE COMMENT; do
+while read -r PROPERTY_C VALUE REST; do
     PROPERTY="${PROPERTY_C%:*}"
     case "$PROPERTY" in
         color_[01][0-9] | background | foreground | cursor)
             THEME_COLORS+=(["$PROPERTY"]="${VALUE:2:6}")
+            ;;
+        name)
+            THEME_NAME="$VALUE ${REST[*]}" # Making a lot of assumptions here
+            THEME_NAME="${THEME_NAME#*\'}"
+            THEME_NAME="${THEME_NAME%\'*}"
             ;;
     esac
 done
@@ -29,12 +34,17 @@ color-print() {
 }
 
 BG="${THEME_COLORS[background]}"
-for ((i=1; i<=8; i++)); do
+FG="${THEME_COLORS[foreground]}"
+
+color-print "$FG" "$BG" " === $THEME_NAME === "
+echo
+
+for ((i = 1; i <= 8; i++)); do
     printf -v PROPERTY "color_%02d" $i
     FG="${THEME_COLORS[$PROPERTY]}"
     color-print "$FG" "$BG" " $i $FG "
     color-print "$BG" "$FG" " $i $FG "
-    printf -v PROPERTY "color_%02d" $((i+8))
+    printf -v PROPERTY "color_%02d" $((i + 8))
     FG="${THEME_COLORS[$PROPERTY]}"
     color-print "$FG" "$BG" " $i $FG "
     echo
