df0cc79544ac60d1aa35cfd111a5b09f9ea40cd3
Artur Rocha <adsr@poczta.onet.pl>
2026-07-22 21:24:20 +0200

    Smarter formatting, on the way to decorations

--- gogh-preview.sh
+++ gogh-preview.sh
@@ -20,6 +20,18 @@ while read -r PROPERTY_C VALUE REST; do
     esac
 done
 
+COLOR_NAMES=(
+    ""
+    "black *"
+    "red ***"
+    "green *"
+    "yellow "
+    "blue **"
+    "magenta"
+    "cyan **"
+    "white *"
+)
+
 color-print() {
     local FG_R="0x${1:0:2}"
     local FG_G="0x${1:2:2}"
@@ -36,16 +48,25 @@ color-print() {
 BG="${THEME_COLORS[background]}"
 FG="${THEME_COLORS[foreground]}"
 
-color-print "$FG" "$BG" " === $THEME_NAME === "
+WIDTH=$((3 * (7 + 6 + 3)))
+printf -v CENTERED_NAME "%*s" $(((${#THEME_NAME} + WIDTH) / 2)) "$THEME_NAME"
+
+# TODO frames/decorations
+color-print "$FG" "$BG" "$CENTERED_NAME"
 echo
 
 for ((i = 1; i <= 8; i++)); do
     printf -v PROPERTY "color_%02d" $i
-    FG="${THEME_COLORS[$PROPERTY]}"
-    color-print "$FG" "$BG" " $i $FG "
-    color-print "$BG" "$FG" " $i $FG "
+    FG_T="${THEME_COLORS[$PROPERTY]}"
+    COLOR_NAME="${COLOR_NAMES[i]}"
+    color-print "$FG_T" "$BG" " $COLOR_NAME $FG_T "
+    color-print "$BG" "$FG_T" " $COLOR_NAME $FG_T "
     printf -v PROPERTY "color_%02d" $((i + 8))
-    FG="${THEME_COLORS[$PROPERTY]}"
-    color-print "$FG" "$BG" " $i $FG "
+    FG_T="${THEME_COLORS[$PROPERTY]}"
+    color-print "$FG_T" "$BG" " $COLOR_NAME $FG_T "
     echo
 done
+
+color-print "${THEME_COLORS[foreground]}" "$BG" 'preview@gogh:~$ '
+color-print "${THEME_COLORS[cursor]}" "$BG" $'cursor \u2588      '
+echo
