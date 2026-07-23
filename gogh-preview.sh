5549496d9d7e97d55182fc8976b5720b79a4ef8b
Artur Rocha <adsr@poczta.onet.pl>
2026-07-23 18:10:19 +0200

    Standardized formatting

--- gogh-preview.sh
+++ gogh-preview.sh
@@ -3,47 +3,47 @@
 declare -A THEME_COLORS
 
 while read -r PROPERTY_C VALUE REST; do
-    PROPERTY="${PROPERTY_C%:*}"
-    case "$PROPERTY" in
-        color_[01][0-9] | background | foreground | cursor)
-            THEME_COLORS+=(["$PROPERTY"]="${VALUE:2:6}")
-            ;;
-        name)
-            THEME_NAME="$VALUE ${REST[*]}" # Making a lot of assumptions here
-            THEME_NAME="${THEME_NAME#*\'}"
-            THEME_NAME="${THEME_NAME%\'*}"
-            ;;
-    esac
+  PROPERTY="${PROPERTY_C%:*}"
+  case "$PROPERTY" in
+    color_[01][0-9] | background | foreground | cursor)
+      THEME_COLORS+=(["$PROPERTY"]="${VALUE:2:6}")
+      ;;
+    name)
+      THEME_NAME="$VALUE ${REST[*]}" # Making a lot of assumptions here
+      THEME_NAME="${THEME_NAME#*\'}"
+      THEME_NAME="${THEME_NAME%\'*}"
+      ;;
+  esac
 done
 
 declare -ra COLOR_NAMES=(
-    ""
-    "black *"
-    "red ***"
-    "green *"
-    "yellow "
-    "blue **"
-    "magenta"
-    "cyan **"
-    "white *"
+  ""
+  "black *"
+  "red ***"
+  "green *"
+  "yellow "
+  "blue **"
+  "magenta"
+  "cyan **"
+  "white *"
 )
 
 color-print() {
-    local FG_R="0x${1:0:2}"
-    local FG_G="0x${1:2:2}"
-    local FG_B="0x${1:4:2}"
+  local FG_R="0x${1:0:2}"
+  local FG_G="0x${1:2:2}"
+  local FG_B="0x${1:4:2}"
 
-    local BG_R="0x${2:0:2}"
-    local BG_G="0x${2:2:2}"
-    local BG_B="0x${2:4:2}"
+  local BG_R="0x${2:0:2}"
+  local BG_G="0x${2:2:2}"
+  local BG_B="0x${2:4:2}"
 
-    printf "\033[38;2;%d;%d;%d;48;2;%d;%d;%dm%s\033[0m" \
-        "$FG_R" "$FG_G" "$FG_B" "$BG_R" "$BG_G" "$BG_B" "$3"
+  printf "\033[38;2;%d;%d;%d;48;2;%d;%d;%dm%s\033[0m" \
+    "$FG_R" "$FG_G" "$FG_B" "$BG_R" "$BG_G" "$BG_B" "$3"
 }
 
 color-println() {
-    color-print "$@"
-    echo
+  color-print "$@"
+  echo
 }
 
 declare -r BG="${THEME_COLORS[background]}"
@@ -58,15 +58,15 @@ color-println "$FG" "$BG" " $ gogh-preview $PADDED_NAME"
 color-println "$FG" "$BG" "$BLANK"
 
 for ((i = 1; i <= 8; i++)); do
-    printf -v PROPERTY "color_%02d" $i
-    FG_T="${THEME_COLORS[$PROPERTY]}"
-    COLOR_NAME="${COLOR_NAMES[i]}"
-    color-print "$FG_T" "$BG" " $COLOR_NAME $FG_T "
-    color-print "$BG" "$FG_T" " $COLOR_NAME $FG_T "
-    printf -v PROPERTY "color_%02d" $((i + 8))
-    FG_T="${THEME_COLORS[$PROPERTY]}"
-    color-print "$FG_T" "$BG" " $COLOR_NAME $FG_T "
-    echo
+  printf -v PROPERTY "color_%02d" $i
+  FG_T="${THEME_COLORS[$PROPERTY]}"
+  COLOR_NAME="${COLOR_NAMES[i]}"
+  color-print "$FG_T" "$BG" " $COLOR_NAME $FG_T "
+  color-print "$BG" "$FG_T" " $COLOR_NAME $FG_T "
+  printf -v PROPERTY "color_%02d" $((i + 8))
+  FG_T="${THEME_COLORS[$PROPERTY]}"
+  color-print "$FG_T" "$BG" " $COLOR_NAME $FG_T "
+  echo
 done
 
 color-println "$FG" "$BG" "$BLANK"
